9bbd442de3170c0c4655079d2cbb0e1d57ba5eaf
user <chantr_2@icloud.com>
2022-11-18 19:19:49 +0300

    user

--- raspisanie/main.py
+++ raspisanie/main.py
@@ -15,28 +15,46 @@ def get_data():
     soup = BeautifulSoup(response.text, "lxml")
     timetable_dict = {}
 
-    timetable = soup.find("tbody").find_all("tr", vl="14.11.2022")
-
-    for i in range(30):
-
-        timetable_title = timetable[i].text
-        print(timetable_title)
-        timetable_dict["1"] = {
-            # "timetable_date": timetable_date,
-            "timetable_title": timetable_title
+    timetable = soup.find("tbody").find_all("tr", vl="21.11.2022")
+    for i in range(10):
+        id = timetable[i].text
+        timetable_dict = {
+            "qwe": id
         }
-        with open("timetable_dict.json", "w") as file:
-            json.dump(timetable_dict, file, indent=2, ensure_ascii=False)
+    with open("otus.txt", "w", encoding='utf8') as file:
+        file.write(id)
+        print(id)
+
+
+    # for a_id in range(20):
+    #
+    #     for i in range(10):
+    #
+    #         timetable_title = timetable[i].text
+    #
+    #         # try:
+    #         #     timetable_title = timetable[i].text
+    #         # except:
+    #         #     timetable_title = "qwe"
+    #
+    #         timetable_dict[a_id] = {
+    #             "timetable_title": timetable_title
+    #         }
+    #         with open("timetable_dict.json", "w") as file:
+    #             json.dump(timetable_dict, file, indent=5, ensure_ascii=False)
+    #         print(timetable_title)
+    # if timetable_title !="qwe":
+    #     with open("timetable_dict.json", "w") as file:
+    #         json.dump(timetable_dict, file, indent=5, ensure_ascii=False)
+    # else:
+    #     print("error")
 
         # try:
         #     timetable_date = timetable[0].text
         # except:
         #     timetable_date = "rqwe"
 
-        # try:
-        #     timetable_title = timetable[i].text
-        # except:
-        #     timetable_title = "qwe"
+
 
 def main():
     get_data()
@@ -44,3 +62,58 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+# timetable_title0 = timetable[0].text
+# timetable_title1 = timetable[1].text
+# timetable_title2 = timetable[2].text
+# timetable_title3 = timetable[3].text
+# timetable_title4 = timetable[4].text
+# timetable_title5 = timetable[5].text
+# timetable_title6 = timetable[6].text
+# timetable_title7 = timetable[7].text
+# timetable_title8 = timetable[8].text
+# timetable_title9 = timetable[9].text
+# timetable_title10 = timetable[10].text
+# timetable_title11 = timetable[11].text
+# timetable_title12 = timetable[12].text
+# timetable_title13 = timetable[13].text
+# timetable_title14 = timetable[14].text
+# timetable_title15 = timetable[15].text
+# timetable_title16 = timetable[16].text
+# timetable_title17 = timetable[17].text
+# timetable_title18 = timetable[18].text
+# timetable_title19 = timetable[19].text
+# timetable_title20 = timetable[20].text
+# timetable_title21 = timetable[21].text
+# timetable_title22 = timetable[22].text
+# timetable_title23 = timetable[23].text
+# timetable_title24 = timetable[24].text
+# timetable_title25 = timetable[25].text
+
+
+# "timetable_title0": timetable_title0,
+# "timetable_title1": timetable_title1,
+# "timetable_title2": timetable_title2,
+# "timetable_title3": timetable_title3,
+# "timetable_title4": timetable_title4,
+# "timetable_title5": timetable_title5,
+# "timetable_title6": timetable_title6,
+# "timetable_title7": timetable_title7,
+# "timetable_title8": timetable_title8,
+# "timetable_title9": timetable_title9,
+# "timetable_title10": timetable_title10,
+# "timetable_title11": timetable_title11,
+# "timetable_title12": timetable_title12,
+# "timetable_title13": timetable_title13,
+# "timetable_title14": timetable_title14,
+# "timetable_title15": timetable_title15,
+# "timetable_title16": timetable_title16,
+# "timetable_title17": timetable_title17,
+# "timetable_title18": timetable_title18,
+# "timetable_title19": timetable_title19,
+# "timetable_title20": timetable_title20,
+# "timetable_title21": timetable_title21,
+# "timetable_title22": timetable_title22,
+# "timetable_title23": timetable_title23
+# "timetable_title24": timetable_title24,
+# "timetable_title25": timetable_title25
--- raspisanie/tg_bot.py
+++ raspisanie/tg_bot.py
@@ -23,8 +23,30 @@ async def timetable(message: types.Message):
         timetable_dict = json.load(file)
 
     for k, v in (timetable_dict.items()):
-        rasp = f"\n{hunderline(v['timetable_date'])}" \
-               f"\n{hcode(v['timetable_title'])}"
+        rasp = f"\n{hbold(v['timetable_title0'])}" \
+               f"\n{hcode(v['timetable_title1'])}"\
+               f"\n{hcode(v['timetable_title2'])}"\
+               f"\n{hbold(v['timetable_title3'])}"\
+               f"\n{hcode(v['timetable_title4'])}"\
+               f"\n{hcode(v['timetable_title5'])}"\
+               f"\n{hcode(v['timetable_title6'])}"\
+               f"\n{hbold(v['timetable_title7'])}"\
+               f"\n{hcode(v['timetable_title8'])}"\
+               f"\n{hcode(v['timetable_title9'])}"\
+               f"\n{hcode(v['timetable_title10'])}"\
+               f"\n{hcode(v['timetable_title11'])}"\
+               f"\n{hbold(v['timetable_title12'])}"\
+               f"\n{hcode(v['timetable_title13'])}"\
+               f"\n{hcode(v['timetable_title14'])}"\
+               f"\n{hcode(v['timetable_title15'])}"\
+               f"\n{hcode(v['timetable_title16'])}"\
+               f"\n{hbold(v['timetable_title17'])}"\
+               f"\n{hcode(v['timetable_title18'])}"\
+               f"\n{hcode(v['timetable_title19'])}"\
+               f"\n{hbold(v['timetable_title20'])}"\
+               # f"\n{hcode(v['timetable_title21'])}"\
+               # f"\n{hcode(v['timetable_title22'])}"\
+               # f"\n{hcode(v['timetable_title23'])}"
 
     await message.answer(rasp)
 
