438f7086e4e595758c42662971f627dd80b9fb10
user <chantr_2@icloud.com>
2022-11-28 17:23:34 +0300

    uiser

---
+++ mail_bot/test.py
@@ -0,0 +1,18 @@
+# str = "Hi world"
+# str2 = 'Hi'
+# x = str.split(" ")[0]
+# print(x)
+# for i in x:
+#     if str2 == x:
+#         print("It work")
+#     else:
+# #         print("Dont,work")
+# text = 'Hi word'
+# res = text.split()
+# for i in res:
+#     if i == 'Hi':
+#         print('Its work')
+my_list = [1, 'two', 'a', 4]
+my_list.reverse()  # None
+
+print(my_list)
--- raspisanie/main.py
+++ raspisanie/main.py
@@ -2,6 +2,7 @@ import requests
 import json
 from bs4 import BeautifulSoup
 from pprint import pprint
+ 
 
 
 
